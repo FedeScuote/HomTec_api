a762fbc769dcd59ea6e61f9498f443567dc178ed
FedeScuote <fscuoteguazza@gmail.com>
2015-12-10 15:41:59 -0300

    added config.py and print the config used

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
-app = Flask(__name__)
+import os
 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -13,3 +15,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+    print(os.environ['APP_SETTINGS'])
